bf712aca2382a0b6e5f600f59159edb45593131c
97nirmal <147032940+97nirmal@users.noreply.github.com>
2024-03-02 23:08:30 +0530

    Add files via upload

---
+++ Dem.java
@@ -0,0 +1,110 @@
+//Relational operator
+
+/*class Main
+{
+public static void main(String args [])
+{
+int a=10,b=20;
+System.out.println(a>b);
+System.out.println(a<b);
+System.out.println(a!=b);
+System.out.println(a==b);
+}
+}*/
+
+
+
+//Bitwise operator
+
+/*class Main
+{
+public static void main(String args [])
+{
+int a=10,b=7;
+System.out.println(a&b);
+System.out.println(a|b);
+System.out.println(a^b);
+}
+}*/
+
+
+
+
+// Logical operator
+
+/*class Main
+{
+public static void main(String args []);
+{
+
+int a=10,b=5,c=15;
+
+System.out.println((a>b)&&(a>c));
+
+System.out.println((a>b)&&(a<c));
+
+System.out.println((b>a)||(b>c));
+}
+}*/
+
+
+
+//Switch Statement
+
+import java.util.*;
+
+
+class Dem
+{
+public static void main(String args[])
+{
+int num;
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter a number");
+num=sc.nextInt();
+
+switch(num)
+{
+case 1:
+System.out.println("First");
+break;
+
+case 2:
+System.out.println("Second");
+break;
+
+case 3:
+System.out.println("Third");
+break;
+
+default:
+System.out.println("Invalid");
+}
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
